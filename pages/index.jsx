9d4450bc123536e810b513fa287655ce95444639
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-04-01 17:53:49 +0530

    added-nav

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Nav from "@/components/Nav";
 
 export default function Home() {
 	return (
-		<main
-			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-		>
-			Blink of AI
+		<main className="">
+			<Nav />
 		</main>
 	);
 }
